fb5ca682515b680aa00e9e3d5c1e00fd52d1c375
Loagn Randolph <loganrandolph@gmail.com>
2014-01-01 20:15:09 -0500

    add js for daytracker & js folder

---
+++ js/daytracker.js
@@ -0,0 +1,10 @@
+
+today=new Date()
+
+thisDay=today.getDay()
+
+var myDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+thisDay=myDays[thisDay]
+
+document.write(thisDay)
